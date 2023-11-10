025d5d51ba800131426619d658546952359bd86b
2017Yasu <yasu.nkym.96@gmail.com>
2023-11-10 13:54:55 +0900

    make a simple http server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello world')
+import * as http from 'http';
+
+const port = 3000;
+
+const server = http.createServer(
+    (request, response) => {
+        response.end('Hello! Node.js with TypeScript')
+    }
+)
+
+server.listen(port);
+
+console.log(`Access http://localhost:${port}`)
